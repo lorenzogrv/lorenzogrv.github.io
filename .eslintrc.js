5ee66df4366fde30ec765765044875673fc0abf0
Lorenzo García Rivera <lorenzogrv@gmail.com>
2023-12-10 06:53:44 +0100

    feat: rework navigation with hamburger, simplify

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+// @see https://ota-meshi.github.io/eslint-plugin-astro/user-guide/
+module.exports = {
+  // ...
+  extends: [
+    // ...
+    'eslint-config-standard',
+    'plugin:astro/recommended'
+  ],
+  // ...
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ['*.astro'],
+      // Allows Astro components to be parsed.
+      parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      }
+    }
+    // ...
+  ]
+}
